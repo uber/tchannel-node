shawnburke/tchannel

Rename PreferOutgoing{Handler => ""}

node/peer.js
@@ -30,7 +30,7 @@ var TChannelConnection = require('./connection');
 var errors = require('./errors');
 var states = require('./states');
 var Request = require('./request');
-var PreferOutgoingHandler = require('./peer_score_straetiges.js').PreferOutgoingHandler;
+var PreferOutgoing = require('./peer_score_straetiges.js').PreferOutgoing;
 
 var DEFAULT_REPORT_INTERVAL = 1000;
 
@@ -47,7 +47,7 @@ function TChannelPeer(channel, hostPort, options) {
 
     assert(hostPort !== '0.0.0.0:0', 'Cannot create ephemeral peer');
 
-    self.scoreStrategy = self.options.scoreStrategy || PreferOutgoingHandler;
+    self.scoreStrategy = self.options.scoreStrategy || PreferOutgoing;
     self.channel = channel;
     self.logger = self.channel.logger;
     self.timers = self.channel.timers;

node/peer_score_strategies.js
@@ -20,38 +20,38 @@
 
 'use strict';
 
-module.exports.PreferOutgoingHandler = PreferOutgoingHandler;
+module.exports.PreferOutgoing = PreferOutgoing;
 
-function PreferOutgoingHandler(peer) {
+function PreferOutgoing(peer) {
     var self = this;
 
     self.peer = peer;
     self.lastQOS = self.getQOS();
 }
 
-PreferOutgoingHandler.UNCONNECTED = 0;
-PreferOutgoingHandler.ONLY_INCOMING = 1;
-PreferOutgoingHandler.FRESH_OUTGOING = 2;
-PreferOutgoingHandler.READY_OUTGOING = 3;
+PreferOutgoing.UNCONNECTED = 0;
+PreferOutgoing.ONLY_INCOMING = 1;
+PreferOutgoing.FRESH_OUTGOING = 2;
+PreferOutgoing.READY_OUTGOING = 3;
 
-PreferOutgoingHandler.prototype.getQOS = function getQOS() {
+PreferOutgoing.prototype.getQOS = function getQOS() {
     var self = this;
 
     var inconn = self.peer.getInConnection();
     var outconn = self.peer.getOutConnection();
 
     if (!inconn && !outconn) {
-        return PreferOutgoingHandler.UNCONNECTED;
+        return PreferOutgoing.UNCONNECTED;
     } else if (!outconn || outconn.direction !== 'out') {
-        return PreferOutgoingHandler.ONLY_INCOMING;
+        return PreferOutgoing.ONLY_INCOMING;
     } else if (outconn.remoteName === null) {
-        return PreferOutgoingHandler.FRESH_OUTGOING;
+        return PreferOutgoing.FRESH_OUTGOING;
     } else {
-        return PreferOutgoingHandler.READY_OUTGOING;
+        return PreferOutgoing.READY_OUTGOING;
     }
 };
 
-PreferOutgoingHandler.prototype.shouldRequest = function shouldRequest() {
+PreferOutgoing.prototype.shouldRequest = function shouldRequest() {
     var self = this;
 
     // space:
@@ -63,16 +63,16 @@ PreferOutgoingHandler.prototype.shouldRequest = function shouldRequest() {
         self.lastQOS = qos;
     }
     switch (qos) {
-        case PreferOutgoingHandler.ONLY_INCOMING:
+        case PreferOutgoing.ONLY_INCOMING:
             if (!self.peer.channel.destroyed) {
                 self.peer.connect();
             }
             /* falls through */
-        case PreferOutgoingHandler.UNCONNECTED:
+        case PreferOutgoing.UNCONNECTED:
             /* falls through */
-        case PreferOutgoingHandler.FRESH_OUTGOING:
+        case PreferOutgoing.FRESH_OUTGOING:
             return 0.1 + random * 0.3;
-        case PreferOutgoingHandler.READY_OUTGOING:
+        case PreferOutgoing.READY_OUTGOING:
             return 0.4 + random * 0.6;
     }
 };

uber/tchannel-node

peer.js
@@ -28,7 +28,7 @@ var net = require('net');
 var TChannelConnection = require('./connection');
 var errors = require('./errors');
 var Request = require('./request');
-var PreferOutgoingHandler = require('./peer_score_strategies.js').PreferOutgoingHandler;
+var PreferOutgoing = require('./peer_score_strategies.js').PreferOutgoing;
 
 var DEFAULT_REPORT_INTERVAL = 1000;
 
@@ -62,7 +62,7 @@ function TChannelPeer(channel, hostPort, options) {
         );
     }
 
-    self.setScoreStrategy(PreferOutgoingHandler);
+    self.setScoreStrategy(PreferOutgoing);
 
     function onReport() {
         if (!self.hostPort) {

peer_score_strategies.js
@@ -20,39 +20,38 @@
 
 'use strict';
 
-module.exports.PreferOutgoingHandler = PreferOutgoingHandler;
+module.exports.PreferOutgoing = PreferOutgoing;
 
-function PreferOutgoingHandler(peer) {
+function PreferOutgoing(peer) {
     var self = this;
 
     self.peer = peer;
     self.lastTier = self.getTier();
 }
 
+PreferOutgoing.UNCONNECTED = 0;
+PreferOutgoing.ONLY_INCOMING = 1;
+PreferOutgoing.FRESH_OUTGOING = 2;
+PreferOutgoing.READY_OUTGOING = 3;
 
-PreferOutgoingHandler.UNCONNECTED = 0;
-PreferOutgoingHandler.ONLY_INCOMING = 1;
-PreferOutgoingHandler.FRESH_OUTGOING = 2;
-PreferOutgoingHandler.READY_OUTGOING = 3;
-
-PreferOutgoingHandler.prototype.getTier = function getTier() {
+PreferOutgoing.prototype.getTier = function getTier() {
     var self = this;
 
     var inconn = self.peer.getInConnection();
     var outconn = self.peer.getIdentifiedOutConnection();
 
     if (!inconn && !outconn) {
-        return PreferOutgoingHandler.UNCONNECTED;
+        return PreferOutgoing.UNCONNECTED;
     } else if (!outconn || outconn.direction !== 'out') {
-        return PreferOutgoingHandler.ONLY_INCOMING;
+        return PreferOutgoing.ONLY_INCOMING;
     } else if (outconn.remoteName === null) {
-        return PreferOutgoingHandler.FRESH_OUTGOING;
+        return PreferOutgoing.FRESH_OUTGOING;
     } else {
-        return PreferOutgoingHandler.READY_OUTGOING;
+        return PreferOutgoing.READY_OUTGOING;
     }
 };
 
-PreferOutgoingHandler.prototype.getScore = function getScore() {
+PreferOutgoing.prototype.getScore = function getScore() {
     var self = this;
 
     // space:
@@ -62,16 +61,16 @@ PreferOutgoingHandler.prototype.getScore = function getScore() {
     var tier = self.getTier();
     self.lastTier = tier;
     switch (tier) {
-        case PreferOutgoingHandler.ONLY_INCOMING:
+        case PreferOutgoing.ONLY_INCOMING:
             if (!self.peer.channel.destroyed) {
                 self.peer.connect(true);
             }
             /* falls through */
-        case PreferOutgoingHandler.UNCONNECTED:
+        case PreferOutgoing.UNCONNECTED:
             /* falls through */
-        case PreferOutgoingHandler.FRESH_OUTGOING:
+        case PreferOutgoing.FRESH_OUTGOING:
             return 0.1 + random * 0.3;
-        case PreferOutgoingHandler.READY_OUTGOING:
+        case PreferOutgoing.READY_OUTGOING:
             return 0.4 + random * 0.6;
     }
 };