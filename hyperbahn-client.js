7398c4dd2fb90238026d9986c7d97e9f6610456f
Anson Qian <abq@uber.com>
2015-09-22 15:22:11 -0700

    Use default retry limit (5) so non-draining peers can potentially be selected

--- hyperbahn-client.js
+++ hyperbahn-client.js
@@ -264,7 +264,6 @@ function sendRequest(opts, endpoint, cb) {
         timeout: (opts && opts.timeout) || self.defaultTimeout,
         hasNoParent: true,
         trace: false,
-        retryLimit: 1,
         headers: {
             cn: self.callerName
         }
