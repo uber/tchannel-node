1ad9f22ba1f0dc4231650dfa31a0b1ea8f723f7b
Raynos <raynos2@gmail.com>
2015-10-06 18:07:56 -0700

    only emit relay.latency after writing bytes to socket

--- relay_handler.js
+++ relay_handler.js
@@ -562,18 +562,18 @@ RelayRequest.prototype.onIdentified = function onIdentified() {
     self.outreq.responseEvent.on(onResponse);
     self.outreq.errorEvent.on(self.boundOnError);
 
+    if (self.outreq.streamed) {
+        self.outreq.sendStreams(self.inreq.arg1, self.inreq.arg2, self.inreq.arg3);
+    } else {
+        self.outreq.send(self.inreq.arg1, self.inreq.arg2, self.inreq.arg3);
+    }
+
     self.channel.emitFastStat(self.channel.buildStat(
         'tchannel.relay.latency',
         'timing',
         elapsed,
         {}
     ));
-
-    if (self.outreq.streamed) {
-        self.outreq.sendStreams(self.inreq.arg1, self.inreq.arg2, self.inreq.arg3);
-    } else {
-        self.outreq.send(self.inreq.arg1, self.inreq.arg2, self.inreq.arg3);
-    }
 
     function onResponse(res) {
         self.onResponse(res);
