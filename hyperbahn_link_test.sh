671e64c7c77f8ea0bada87d3a204676cf0f4419f
Joshua T Corbin <joshua@uber.com>
2015-10-14 14:28:33 -0700

    hyperbahn_link_test: export a transformed $TCHANNEL_TEST_CONFIG

--- hyperbahn_link_test.sh
+++ hyperbahn_link_test.sh
@@ -8,6 +8,13 @@ else
     dest_dir=$1
 fi
 
+if [ -n "$TCHANNEL_TEST_CONFIG" ]; then
+    dest_tchannel_test_config="$dest_dir/from_tchannel_$(echo "$TCHANNEL_TEST_CONFIG" | tr '/' '_')"
+    cp -fv "$TCHANNEL_TEST_CONFIG" "$dest_tchannel_test_config"
+    TCHANNEL_TEST_CONFIG=$(basename "$dest_tchannel_test_config")
+    export TCHANNEL_TEST_CONFIG
+fi
+
 npm link
 
 cd "$dest_dir"
