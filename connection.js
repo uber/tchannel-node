6481faed8ba4866b2fc8eeddfa0996c9521e5059
Raynos <raynos2@gmail.com>
2015-10-26 01:31:02 -0700

    super-fast-frame-parser

--- connection.js
+++ connection.js
@@ -21,12 +21,12 @@
 'use strict';
 
 var assert = require('assert');
-var bufrw = require('bufrw');
 var extend = require('xtend');
-var ReadMachine = require('bufrw/stream/read_machine');
 var inherits = require('util').inherits;
+var bufrw = require('bufrw');
 var stat = require('./lib/stat.js');
 
+var FrameParser = require('./frame-parser.js');
 var v2 = require('./v2');
 var errors = require('./errors');
 var States = require('./reqres_states');
@@ -85,7 +85,8 @@ function TChannelConnection(channel, socket, direction, socketRemoteAddr) {
     // jshint forin:true
     self.handler = new v2.Handler(opts);
 
-    self.mach = ReadMachine(bufrw.UInt16BE, v2.Frame.RW);
+    self.frameBufferParser = new FrameParser(self, onFrameBuffer);
+    self.frameParser = v2.Frame.RW;
 
     self.setupSocket();
     self.setupHandler();
@@ -97,6 +98,10 @@ function TChannelConnection(channel, socket, direction, socketRemoteAddr) {
 }
 inherits(TChannelConnection, TChannelConnectionBase);
 
+function onFrameBuffer(connection, frameBuffer) {
+    connection.onFrameBuffer(frameBuffer);
+}
+
 TChannelConnection.prototype.setLazyHandling = function setLazyHandling(enabled) {
     var self = this;
 
@@ -104,12 +109,25 @@ TChannelConnection.prototype.setLazyHandling = function setLazyHandling(enabled)
     // boundary should just be self.handler.handleChunk in
     // onSocketChunk under setupSocket; then the switching logic
     // moves wholly into a `self.handler.setLazyHandling(bool)`
-    if (enabled && self.mach.chunkRW !== v2.LazyFrame.RW) {
-        self.mach.chunkRW = v2.LazyFrame.RW;
-    } else if (!enabled && self.mach.chunkRW !== v2.Frame.RW) {
-        self.mach.chunkRW = v2.Frame.RW;
+    if (enabled && self.frameParser !== v2.LazyFrame.RW) {
+        self.frameParser = v2.LazyFrame.RW;
+    } else if (!enabled && self.frameParser !== v2.Frame.RW) {
+        self.frameParser = v2.Frame.RW;
     }
     self.handler.useLazyFrames(enabled);
+};
+
+TChannelConnection.prototype.onFrameBuffer =
+function onFrameBuffer(frameBuffer) {
+    var self = this;
+
+    var res = bufrw.fromBufferResult(self.frameParser, frameBuffer, 0);
+    if (res.err) {
+        self.sendProtocolError('read', res.err);
+        return;
+    }
+
+    self.handleReadFrame(res.value);
 };
 
 TChannelConnection.prototype.setupSocket = function setupSocket() {
@@ -126,10 +144,7 @@ TChannelConnection.prototype.setupSocket = function setupSocket() {
 
     // TODO: move to method for function optimization
     function onSocketChunk(chunk) {
-        var err = self.mach.handleChunk(chunk);
-        if (err) {
-            self.sendProtocolError('read', err);
-        }
+        self.frameBufferParser.write(chunk);
     }
 
     // TODO: move to method for function optimization
@@ -165,8 +180,6 @@ TChannelConnection.prototype.setupHandler = function setupHandler() {
     self.handler.write = function write(buf, done) {
         self.socket.write(buf, null, done);
     };
-
-    self.mach.emit = handleReadFrame;
 
     self.handler.writeErrorEvent.on(onWriteError);
     self.handler.errorEvent.on(onHandlerError);
@@ -206,10 +219,6 @@ TChannelConnection.prototype.setupHandler = function setupHandler() {
 
     function onErrorFrame(errFrame) {
         self.onErrorFrame(errFrame);
-    }
-
-    function handleReadFrame(frame) {
-        self.handleReadFrame(frame);
     }
 
     function onCallRequest(req) {
---
+++ frame-parser.js
@@ -0,0 +1,148 @@
+'use strict';
+
+var Buffer = require('buffer').Buffer;
+var assert = require('assert');
+
+var SIZE_BYTE_LENGTH = 2;
+
+module.exports = FrameParser;
+
+function FrameParser(context, onFrameBuffer) {
+    if (!(this instanceof FrameParser)) {
+        return new FrameParser(context, onFrameBuffer);
+    }
+
+    var self = this;
+
+    self.remainderBuffer = null;
+    self.hasTempRemainderBuffer = false;
+    self.remainderOffset = 0;
+
+    self.frameLength = 0;
+
+    self._context = context;
+    self._onFrameBuffer = onFrameBuffer;
+}
+
+/*eslint max-statements: [2, 25]*/
+FrameParser.prototype.write =
+function write(networkBuffer) {
+    var self = this;
+
+    var networkBufferLength = networkBuffer.length;
+    assert(networkBufferLength > 0, 'Cannot write() empty buffer');
+
+    var maximumBytesAvailable = self.remainderOffset + networkBufferLength;
+    if (maximumBytesAvailable < SIZE_BYTE_LENGTH) {
+        self._addRemainder(networkBuffer, 0, networkBufferLength);
+        return;
+    }
+
+    if (self.frameLength === 0) {
+        self._readInitialFrameLength(networkBuffer);
+    }
+
+    if (self.frameLength > maximumBytesAvailable) {
+        self._addRemainder(networkBuffer, 0, networkBufferLength);
+        return;
+    }
+
+    var startOfBuffer = 0;
+
+    while (self.frameLength <= maximumBytesAvailable) {
+        var amountToRead = self.frameLength - self.remainderOffset;
+        var endOfBuffer = startOfBuffer + amountToRead;
+
+        self._pushFrameBuffer(networkBuffer, startOfBuffer, endOfBuffer);
+        startOfBuffer = endOfBuffer;
+
+        if (networkBufferLength - startOfBuffer < SIZE_BYTE_LENGTH) {
+            break;
+        }
+
+        maximumBytesAvailable = networkBufferLength - startOfBuffer;
+        self.frameLength = networkBuffer.readUInt16BE(startOfBuffer);
+    }
+
+    if (startOfBuffer < networkBufferLength) {
+        self._addRemainder(networkBuffer, startOfBuffer, networkBufferLength);
+    }
+};
+
+FrameParser.prototype._addRemainder =
+function _addRemainder(networkBuffer, start, end) {
+    var self = this;
+
+    if (self.frameLength === 0) {
+        // Maybe allocate a new FastBuffer (cheap)
+        var rawFrameBuffer = maybeSlice(networkBuffer, start, end);
+
+        assert(self.remainderBuffer === null,
+            'Cannot assign remainderBuffer twice');
+        self.remainderBuffer = rawFrameBuffer;
+        self.remainderOffset = rawFrameBuffer.length;
+        self.hasTempRemainderBuffer = true;
+        return;
+    }
+
+    if (self.remainderBuffer === null || self.hasTempRemainderBuffer) {
+        var oldRemainder = self.remainderBuffer;
+
+        // Allocate a SlowBuffer (expensive)
+        self.remainderBuffer = new Buffer(self.frameLength);
+        self.hasTempRemainderBuffer = false;
+
+        if (oldRemainder) {
+            oldRemainder.copy(self.remainderBuffer, 0);
+        }
+    }
+
+    networkBuffer.copy(self.remainderBuffer, self.remainderOffset, start, end);
+    self.remainderOffset += (end - start);
+};
+
+FrameParser.prototype._pushFrameBuffer =
+function _pushFrameBuffer(networkBuffer, start, end) {
+    var self = this;
+
+    var frameBuffer;
+    if (self.remainderOffset === 0) {
+        // Maybe allocate a new FastBuffer (cheap)
+        frameBuffer = maybeSlice(networkBuffer, start, end);
+    } else {
+        self._addRemainder(networkBuffer, start, end);
+
+        frameBuffer = self.remainderBuffer;
+
+        self.remainderBuffer = null;
+        self.hasTempRemainderBuffer = false;
+        self.remainderOffset = 0;
+    }
+
+    self._onFrameBuffer(self._context, frameBuffer);
+    self.frameLength = 0;
+};
+
+FrameParser.prototype._readInitialFrameLength =
+function _readInitialFrameLength(networkBuffer) {
+    var self = this;
+
+    if (self.remainderOffset === 0) {
+        self.frameLength = networkBuffer.readUInt16BE(0);
+    } else if (self.remainderOffset === 1) {
+        self.frameLength = self.remainderBuffer[0] << 8 | networkBuffer[0];
+    } else if (self.remainderOffset >= 2) {
+        self.frameLength = self.remainderBuffer.readUInt16BE(0);
+    }
+};
+
+function maybeSlice(buf, start, end) {
+    var slice;
+    if (start === 0 && end === buf.length) {
+        slice = buf;
+    } else {
+        slice = buf.slice(start, end);
+    }
+
+    return slice;
+}
