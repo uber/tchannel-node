5fb6c6692ca965924a84b7124778098f9115cc47
Raynos <raynos2@gmail.com>
2015-11-01 19:25:49 -0800

    linting: [out_request] comply with complexity rule

--- out_request.js
+++ out_request.js
@@ -32,7 +32,7 @@ var errors = require('./errors');
 var States = require('./reqres_states');
 
 function TChannelOutRequest(id, options) {
-    /*eslint max-statements: [2, 50]*/
+    /*eslint max-statements: [2, 50], complexity: [2, 25]*/
     var self = this;
 
     EventEmitter.call(self);
