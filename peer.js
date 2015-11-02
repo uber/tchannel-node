0d0f190151f32934f7ad619fce4aa5fb744a25f6
Raynos <raynos2@gmail.com>
2015-11-01 19:53:05 -0800

    linting: [peer] comply with no-undef rule

--- peer.js
+++ peer.js
@@ -26,6 +26,8 @@ var inspect = require('util').inspect;
 var EventEmitter = require('./lib/event_emitter');
 var stat = require('./stat-tags.js');
 var net = require('net');
+var process = require('process');
+var CountedReadySignal = require('ready-signal/counted');
 
 var TChannelConnection = require('./connection');
 var errors = require('./errors');
