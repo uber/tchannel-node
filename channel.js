b0edd6c6ad247e6166afc35c81933dbb53a62af4
Raynos <raynos2@gmail.com>
2015-11-02 18:10:13 -0800

    channel: Ensure we do an async deferred loop in sanitySweep

--- channel.js
+++ channel.js
@@ -38,6 +38,7 @@ var extend = require('xtend');
 var globalTimers = extend(require('timers'), {
     now: Date.now
 });
+var setImmediate = require('timers').setImmediate;
 var globalRandom = Math.random;
 var net = require('net');
 var format = require('util').format;
@@ -934,12 +935,31 @@ function sanitySweep(callback) {
     }
 
     var incomingConns = Object.keys(self.serverConnections);
-    for (var i = 0; i < incomingConns.length; i++) {
-        var conn = self.serverConnections[incomingConns[i]];
-        conn.ops.sanitySweep();
-    }
 
-    self.peers.sanitySweep(callback);
+    nextConn(incomingConns, 0, function connSweepDone(err) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        self.peers.sanitySweep(callback);
+    });
+
+    function nextConn(connectionKeys, index, cb) {
+        if (index >= connectionKeys.length) {
+            cb(null);
+            return;
+        }
+
+        var connection = self.serverConnections[connectionKeys[index]];
+        connection.ops.sanitySweep(function opsSweepDone() {
+            setImmediate(deferNextConn);
+        });
+
+        function deferNextConn() {
+            nextConn(connectionKeys, index + 1, cb);
+        }
+    }
 };
 
 TChannel.prototype.setMaxTombstoneTTL =
