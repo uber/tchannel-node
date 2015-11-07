f9b3de099b7a1298db37989d3bc4358cd4012055
Raynos <raynos2@gmail.com>
2015-11-06 18:31:11 -0800

    test both eager & lazy relay

--- test/relay-under-timeouts.js
+++ test/relay-under-timeouts.js
@@ -27,29 +27,20 @@ var allocCluster = require('./lib/alloc-cluster');
 var RelayHandler = require('../relay_handler');
 var CollapsedAssert = require('./lib/collapsed-assert');
 
-allocCluster.test('sending a 1000 requests', {
+allocCluster.test('sending a 1000 requests through eager relay', {
     numPeers: 3
 }, function t(cluster, assert) {
     cluster.logger.whitelist('info', 'expected error while forwarding');
     cluster.logger.whitelist('info', 'forwarding expected error frame');
 
-    // cluster.logger.whitelist('info', 'popOutReq received for unknown or lost id');
-    // cluster.logger.whitelist('info', 'ignoring outresponse.sendError on a closed connection');
-
     var client = cluster.channels[0];
     var relay = cluster.channels[1];
-    var server = cluster.channels[2];
 
-    var subServer = server.makeSubChannel({
-        serviceName: 'server'
-    });
-    var subRelay = relay.makeSubChannel({
-        serviceName: 'server',
-        peers: [server.hostPort]
-    });
-    subRelay.handler = new RelayHandler(subRelay);
+    setupRelay(cluster);
+    setupTimeoutServer(cluster);
 
-    subServer.register('bleed', noop);
+    relay.setLazyHandling(false);
+    relay.setLazyRelaying(false);
 
     var batchClient = BatchClient(client, [relay.hostPort], {
         totalRequests: 1000,
@@ -57,7 +48,6 @@ allocCluster.test('sending a 1000 requests', {
         delay: 50,
         endpoint: 'bleed'
     });
-
     batchClient.sendRequests(onResults);
 
     function onResults(err, data) {
@@ -82,20 +72,80 @@ allocCluster.test('sending a 1000 requests', {
     }
 
     function finish() {
-        console.log('counters', {
-            IN_REQ_ALLOC_COUNTER: IN_REQ_ALLOC_COUNTER,
-            TIMEOUT_COUNTER: TIMEOUT_COUNTER,
-            NOT_SEND_COUNTER: NOT_SEND_COUNTER,
-            POP_IN_REQUEST_COUNTER: POP_IN_REQUEST_COUNTER,
-            EMIT_FINISH_COUNTER: EMIT_FINISH_COUNTER,
-            serverHostPort: server.hostPort,
-            relayHostPort: relay.hostPort
-        });
-
         assert.end();
     }
 });
 
-function noop() {
-    /* timeout on purpose */
+allocCluster.test('sending a 1000 requests through lazy relay', {
+    numPeers: 3
+}, function t(cluster, assert) {
+    cluster.logger.whitelist('info', 'expected error while forwarding');
+    cluster.logger.whitelist('info', 'forwarding expected error frame');
+
+    var client = cluster.channels[0];
+    var relay = cluster.channels[1];
+
+    setupRelay(cluster);
+    setupTimeoutServer(cluster);
+
+    relay.setLazyHandling(true);
+    relay.setLazyRelaying(true);
+
+    var batchClient = BatchClient(client, [relay.hostPort], {
+        totalRequests: 1000,
+        batchSize: 50,
+        delay: 50,
+        endpoint: 'bleed'
+    });
+    batchClient.sendRequests(onResults);
+
+    function onResults(err, data) {
+        assert.ifError(err);
+
+        assert.equal(data.errors.length + data.results.length, 1000);
+
+        var cassert = CollapsedAssert();
+
+        for (var i = 0; i < data.errors.length; i++) {
+            cassert.equal(data.errors[i].type, 'tchannel.request.timeout');
+        }
+
+        for (var j = 0; j < data.results.length; j++) {
+            cassert.equal(data.results[j].responseOk, false);
+            cassert.equal(data.results[j].error.type, 'tchannel.timeout');
+        }
+
+        cassert.report(assert, 'all response are errors');
+
+        setTimeout(finish, 750);
+    }
+
+    function finish() {
+        assert.end();
+    }
+});
+
+function setupRelay(cluster) {
+    var relay = cluster.channels[1];
+    var server = cluster.channels[2];
+
+    var subRelay = relay.makeSubChannel({
+        serviceName: 'server',
+        peers: [server.hostPort]
+    });
+    subRelay.handler = new RelayHandler(subRelay);
+}
+
+function setupTimeoutServer(cluster) {
+    var server = cluster.channels[2];
+
+    var subServer = server.makeSubChannel({
+        serviceName: 'server'
+    });
+
+    subServer.register('bleed', noop);
+
+    function noop() {
+        /* timeout on purpose */
+    }
 }
