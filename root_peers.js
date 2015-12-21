ee5e92f98b276d24561e8a810f09bdc3df35269d
Raynos <raynos2@gmail.com>
2015-12-21 14:36:19 -0800

    root_peers: remove SelfPeer references

--- root_peers.js
+++ root_peers.js
@@ -22,17 +22,14 @@
 
 var inherits = require('util').inherits;
 var extend = require('xtend');
-var setImmediate = require('timers').setImmediate;
 
 var TChannelPeersBase = require('./peers_base.js');
 var TChannelPeer = require('./peer');
-var TChannelSelfPeer = require('./self_peer');
 
 function TChannelRootPeers(channel, options) {
     TChannelPeersBase.call(this, channel, options);
     this.allocPeerEvent = this.defineEvent('allocPeer');
     this.peerOptions = this.options.peerOptions || {};
-    this.selfPeer = null;
 }
 
 inherits(TChannelRootPeers, TChannelPeersBase);
@@ -41,54 +38,7 @@ TChannelRootPeers.prototype.close = function close(callback) {
     var self = this;
 
     var peers = self.values();
-    if (self.selfPeer) {
-        peers.push(self.selfPeer);
-    }
     TChannelPeersBase.prototype.close.call(self, peers, callback);
-};
-
-TChannelRootPeers.prototype.sanitySweep =
-function sanitySweep(callback) {
-    var self = this;
-
-    if (!self.selfPeer) {
-        TChannelPeersBase.prototype.sanitySweep.call(self, callback);
-        return;
-    }
-
-    nextConn(self.selfPeer.connections, 0, function connSweepDone(err) {
-        if (err) {
-            callback(err);
-            return;
-        }
-
-        TChannelPeersBase.prototype.sanitySweep.call(self, callback);
-    });
-
-    function nextConn(connections, index, cb) {
-        if (index >= connections.length) {
-            cb(null);
-            return;
-        }
-
-        var connection = connections[index];
-        connection.ops.sanitySweep(function opsSweepDone() {
-            setImmediate(deferNextConn);
-        });
-
-        function deferNextConn() {
-            nextConn(connections, index + 1, cb);
-        }
-    }
-};
-
-TChannelRootPeers.prototype.getSelfPeer = function getSelfPeer() {
-    var self = this;
-
-    if (!self.selfPeer) {
-        self.selfPeer = new TChannelSelfPeer(self.channel);
-    }
-    return self.selfPeer;
 };
 
 TChannelRootPeers.prototype.add = function add(hostPort, options) {
@@ -98,10 +48,6 @@ TChannelRootPeers.prototype.add = function add(hostPort, options) {
     var peer = self._map[hostPort];
     if (peer) {
         return peer;
-    }
-
-    if (hostPort === self.channel.hostPort) {
-        return self.getSelfPeer();
     }
 
     options = options || extend({}, self.peerOptions);
