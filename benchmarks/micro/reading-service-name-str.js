5a90ed1b55a4885981ef00e3d096bd9e785c88c2
Raynos <raynos2@gmail.com>
2016-01-25 17:02:48 -0800

    add new micro bench

--- benchmarks/micro/reading-service-name-str.js
+++ benchmarks/micro/reading-service-name-str.js
@@ -18,26 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-// Copyright (c) 2015 Uber Technologies, Inc.
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the 'Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 'use strict';
 
 var Buffer = require('buffer').Buffer;
@@ -70,49 +50,65 @@ var frame = new v2.Frame(24,    // frame id
     )
 );
 
-function main(ITER) {
+function main(mode, ITER) {
     var buf = bufrw.toBuffer(v2.Frame.RW, frame);
 
-    runLoop(buf, 1000);
+    runLoop(buf, mode, 1000);
     console.log('done warmup');
     setTimeout(pastWarmup, 250);
 
     function pastWarmup() {
         console.log('running bench', process.pid);
         var start = Date.now();
-        runLoop(buf, ITER);
+        runLoop(buf, mode, ITER);
         var end = Date.now();
         console.log('finised bench', end - start);
     }
 }
 
-function runLoop(buf, ITER) {
+function runLoop(buf, mode, ITER) {
+    if (mode === 'optimized') {
+        runOptimizedLoop(buf, ITER);
+    } else if (mode === 'default') {
+        runDefaultLoop(buf, ITER);
+    } else {
+        console.warn('noop');
+    }
+}
+
+function runDefaultLoop(buf, ITER) {
+    var resArr = new Array(10);
+
+    for (var i = 0; i < ITER; i++) {
+        var lazyFrame = bufrw.fromBuffer(v2.LazyFrame.RW, buf);
+        var res = lazyFrame.bodyRw.lazy.readService(lazyFrame);
+
+        resArr[i % 10] = new FrameData(res.value);
+    }
+}
+
+function runOptimizedLoop(buf, ITER) {
     var resArr = new Array(10);
 
     for (var i = 0; i < ITER; i++) {
         var lazyFrame = bufrw.fromBuffer(v2.LazyFrame.RW, buf);
         var serviceName = lazyFrame.bodyRW.lazy.readServiceStr(lazyFrame);
-        var callerName = lazyFrame.bodyRW.lazy.readCallerNameStr(lazyFrame);
-        var endpoint = lazyFrame.bodyRW.lazy.readArg1Str(lazyFrame);
 
-        resArr[i % 10] = new FrameData(
-            serviceName, callerName, endpoint
-        );
+        resArr[i % 10] = new FrameData(serviceName);
     }
 }
 
-function FrameData(serviceName, callerName, endpoint) {
+function FrameData(serviceName) {
     this.serviceName = serviceName;
-    this.callerName = callerName;
-    this.endpoint = endpoint;
 }
 
 if (require.main === module) {
     var arg = process.argv[2];
+    var mode = process.argv[3] || 'optimized';
     var ITERATIONS = 1000 * 1000 * 10;
     if (arg) {
         ITERATIONS = 1000 * 1000 * parseInt(arg, 10);
     }
 
-    main(ITERATIONS);
+    main(mode, ITERATIONS);
 }
