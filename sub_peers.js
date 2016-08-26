d1cc70efd81431d31a551fbf9c5bdf8a13ad5512
Raynos <jakev@uber.com>
2016-08-26 14:27:25 -0700

    fix lolbug

--- sub_peers.js
+++ sub_peers.js
@@ -58,7 +58,7 @@ function TChannelSubPeers(channel, options) {
     }
 
     function boundOnRefreshConnectedPeers() {
-        this.refreshConnectedPeers();
+        self.refreshConnectedPeers();
     }
 }
 
